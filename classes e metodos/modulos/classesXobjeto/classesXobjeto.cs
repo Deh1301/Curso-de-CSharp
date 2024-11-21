72d1ef4567bb8648453979c8acf8736cc73ba01b
ANDRE CALIXTO DOS SANTOS <129115005+Deh1301@users.noreply.github.com>
2024-11-21 12:17:34 -0300

    atualizando

--- classes e metodos/modulos/classesXobjeto/classesXobjeto.cs
+++ classes e metodos/modulos/classesXobjeto/classesXobjeto.cs
@@ -1,7 +1,7 @@
 ﻿namespace ClassesMetodos{
     public class ClassesXobjeto{
         public static void classeXobjeto(){
-            Console.WriteLine("ola esta funcionando o teste do programa");
+            Console.WriteLine("ola esta funcionando o teste do programa de c#");
             //class define um tipo
 
         }
