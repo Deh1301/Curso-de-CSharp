2de645b11774227e6cc234180af35910a339059b
Andre Calixto <calixtoandre77@gmail.com>
2024-11-21 11:38:24 -0300

    ola esse e curso de c#

---
+++ classes e metodos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassesMetodos;
+
+public class Classes{
+    public static void Main(string[] args){
+        Console.WriteLine("\nOla esse é o modulo de classes deseja Continuar? S/N\n");
+        string entrada = Console.ReadLine();
+        var entradaConfirmar = entrada.ToLower() == "s" ? "Bem Vindo seu exercicio sera executado" : "Voce não Acesso, encerrando";
+        Console.WriteLine($"\n{entradaConfirmar}\n");
+
+        ClassesXobjeto.classeXobjeto();
+    }
+       
+}
---
+++ classes e metodos/modulos/classesXobjeto/classesXobjeto.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassesMetodos{
+    public class ClassesXobjeto{
+        public static void classeXobjeto(){
+            Console.WriteLine("ola");
+            //class define um tipo
+
+        }
+    }
+}
